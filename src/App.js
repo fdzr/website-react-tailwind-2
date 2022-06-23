e7dd0eb7ed3341ab3eccf3c1522e8a1c8709a753
fdzr <fzamora.reina@gmail.com>
2022-06-23 00:11:20 -0400

    adding functionalities

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import Analytics from './components/Analytics';
+import Cards from './components/Cards';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Newsletter from './components/Newsletter';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
     </div>
   );
 }
